300a2d9a672de346e381cf35501429bb334132a3
Omar-Shaikh1542 <tumblingtortoise1542@gmail.com>
2021-02-01 15:29:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// var i_quote = document.getElementById("i-quote");
+// var ii_quote = document.getElementById("ii-quote");
+// var iii_quote = document.getElementById("iii-quote");
+
+// i_quote.addEventListener("click", carousel);
+
+// function carousel(){
+//     i_quote.style.transitionDelay= "4s";
+//     i_quote.style.transitionDuration= "3s";
+//     // i_quote.style.marginLeft = "1000%";
+//     i_quote.style.color = "transparent";
+
+// }
